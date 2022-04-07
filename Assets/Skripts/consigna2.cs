60bcba18849f25b734a4efe0adb351447d1bc740
STERNPORTNOI <101826917+STERNPORTNOI@users.noreply.github.com>
2022-04-07 15:24:03 -0300

    Consigna2
    se creó y resolvió la consigna2

--- Assets/Skripts/consigna2.cs
+++ Assets/Skripts/consigna2.cs
@@ -2,12 +2,39 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+//Crear las variables necesarias para ingresar por Inspector
+// el precio de 3 productos y un monto de dinero disponible. 
+// Comunicar si la suma de los mismos supera o no el monto de dinero disponible. 
+// Luego indicar cuánto dinero sobra o falta.
+
 public class consigna2 : MonoBehaviour
 {
+    public float producto1;
+    public float producto2;
+    public float producto3;
+    public float MontoDisponible;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        float SumaDeProductos = producto1 + producto2 + producto3;
+
+        if (SumaDeProductos > MontoDisponible)
+        {
+            float faltante = SumaDeProductos - MontoDisponible;
+            Debug.Log("La suma de los productos supera el monto disponible. Faltan $" + faltante);
+        }
+
+        if (SumaDeProductos < MontoDisponible)
+        {
+            float sobrante = MontoDisponible - SumaDeProductos;
+            Debug.Log("La suma de los productos no supera el monto disponible. Sobran $" + sobrante);
+        }
+
+        else
+        {
+            Debug.Log("La suma de los productos es igual al monto disponible. No sobra ni falta dinero");
+        }
     }
 
     // Update is called once per frame
