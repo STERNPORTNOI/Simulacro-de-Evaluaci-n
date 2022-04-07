1476ec9050cad485275221fbd47a5b04b7d7fc1e
STERNPORTNOI <101826917+STERNPORTNOI@users.noreply.github.com>
2022-04-07 15:07:04 -0300

    Consigna1
    Consigna1 creada y resuelta

---
+++ Assets/Skripts/consigna1.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Crea un programa que permita ingresar por Inspector 
+// dos valores enteros y responda si son iguales o no lo son.
+
+public class consigna1 : MonoBehaviour
+{
+    public int valor1;
+    public int valor2;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (valor1 == valor2)
+        {
+            Debug.Log("Ambos valores enteros son iguales");
+        }
+
+        else
+        {
+            if (valor1 > valor2)
+            {
+                Debug.Log("Loas valores no son iguales. El valor1 es mayor al valor2");
+            }
+
+            else
+            {
+                Debug.Log("Los valores no son iguales. El valor2 es mayor al valor1");
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
